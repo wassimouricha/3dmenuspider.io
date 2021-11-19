cdfb39da6af3181516b27f4f3e0ed416580fd386
Wassim Bouricha <91595307+wassimouricha@users.noreply.github.com>
2021-11-19 13:15:41 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// on introduit ses variables 
+
+const hamburger_menu = document.querySelector(" .hamburger-menu");
+const container = document.querySelector(" .container");
+
+hamburger_menu.addEventListener("click", () => {
+    container.classList.toggle("active");
+});
+
+
